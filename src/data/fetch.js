cf5dd5844800f5ec2cb0ed1a100a4c4b7ee85447
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-05-21 20:22:03 -0400

    create: functions for getting today date and the date from eigth days ago

--- src/data/fetch.js
+++ src/data/fetch.js
@@ -4,3 +4,8 @@ export function getPictureOfTheDay () {
     return fetch(`https://api.nasa.gov/planetary/apod?api_key=${URL}`)
     .then(res => res.json())
 }
+
+export function getLastestPOTD (today, lastweek) {
+    return fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=${lastweek}&end_date=${today}`)
+    .then(res => res.json())
+}
--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
+
 export default function Archive () {
+
+    function getTodayDate() {
+        const fullDate = new Date();
+        const date = fullDate.toLocaleDateString('en-US').split("/");
+        date.unshift(date.pop());
+        return date.join("/");
+    }
+
+    function getLastWeekDate () {
+        const currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() - 8);
+        let formattedDate = currentDate.toISOString().slice(0, 10);
+        return formattedDate.replaceAll("-","/");
+    }
+
+    console.log(getTodayDate(), getLastWeekDate())
+
+
     return (
         <>
-            <h1>this is the old boring archive</h1>
+            
         </>
     )
 }
